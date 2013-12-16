bb7052a342c9d466a43f58c3e1e0c940ed27c5a6
Drew Butler <hi@dbtlr.com>
2013-12-16 15:15:58 -0500

    Update hooks version number.

--- functions/add_hooks.fish
+++ functions/add_hooks.fish
@@ -1,7 +1,7 @@
 function add_hooks
     mkdir .tmp
     cd .tmp
-	wget https://github.com/dbtlr/git-hooks/archive/1.0.0.tar.gz
+	wget https://github.com/dbtlr/git-hooks/archive/1.0.1.tar.gz
 	tar -xzf 1.0.0.tar.gz
 	cp git-hooks-1.0.0/hooks/* ../.git/hooks/
 	cd ..
